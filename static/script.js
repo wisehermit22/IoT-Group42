7fe6dc5d170c50a171dac8c14d639494647e62dc
Sckaeth <sckaeth@gmail.com>
2024-10-15 10:48:32 +0800

    More even more stuff

--- static/script.js
+++ static/script.js
@@ -10,6 +10,7 @@ socket.on('status_update', function(data) {
     document.getElementById('inventory-count').textContent = data.inventory_count;
     document.getElementById('total-add-count').textContent = data.added_count;
     document.getElementById('total-rem-count').textContent = data.consumption_count;
+    document.getElementById('consumption-limit').textContent = data.consumption_limit;
     document.getElementById('lock-status').textContent = data.lock_status ? 'Locked' : 'Unlocked';
     document.getElementById('lid-status').textContent = data.lid_status ? 'Closed' : 'Open';
     document.getElementById('remaining-time').textContent = data.lockout_remaining;
